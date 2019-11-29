7db1db5bb8985067ef7f2b050f226cb4061c0a0e
Elio transetti <elio.transetti@orange.fr>
2019-11-29 16:00:56 +0100

    elio

---
+++ acceuil_stealmarket.php
@@ -0,0 +1,31 @@
+<!doctype html utf-8>
+
+<html>
+    <head>
+
+    </head>
+
+    <body>
+       
+        <a href="acceuil_stealmarket.php">INDUMENTIS</a> 
+        
+        <a href="View_homme.php">Collection Homme</a> 
+        
+        <a href="View_femme.php">Collection Femme</a> 
+        
+        <a href="View_accessoire.php">Accessoire</a> 
+        
+        <a href="contact.php">En savoir plus</a> 
+
+        <a href="recherche.html"><img src="loupe.png"/>
+        
+        <a href="inscription_steal.php"><img src="engrenage.jpg"/>
+
+        <a href="panier_steal.php"><img src="caddie.jpg"/> <br> <br>
+
+        <a href="View_homme.php"><img src="vetement_homme.jpg"/>
+    
+        <a href="View_femme.php"><img src="vetement_femme.jpg"/> <br>
+    </body>
+
+</html>
