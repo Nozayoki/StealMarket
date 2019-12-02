f65801823ef29fae6c532b11ff53cf9c4dd558fa
William Mpassy <williammpassy@gmail.com>
2019-12-02 22:28:29 +0100

    william image

--- View/View_acceuil_stealmarket.php
+++ View/View_acceuil_stealmarket.php
@@ -17,15 +17,15 @@
         
         <a href="contact.php">En savoir plus</a> 
 
-        <a href="recherche.html"><img src="loupe.png"/>
+        <a href="recherche.html"><img src="Image/loupe.png"/>
         
-        <a href="inscription_steal.php"><img src="engrenage.jpg"/>
+        <a href="inscription_steal.php"><img src="Image/engrenage.jpg"/>
 
-        <a href="panier_steal.php"><img src="caddie.jpg"/> <br> <br>
+        <a href="panier_steal.php"><img src="Image/caddie.jpg"/> <br> <br>
 
-        <a href="View_homme.php"><img src="vetement_homme.jpg"/>
+        <a href="View_homme.php"><img src="Image/vetement_homme.jpg"/>
     
-        <a href="View_femme.php"><img src="vetement_femme.jpg"/> <br>
+        <a href="View_femme.php"><img src="Image/vetement_femme.jpg"/> <br>
 
         <form action="../Control/Control_accueil.php" method="post">
             <p>
