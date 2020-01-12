53180f035112b909d2210b01fdc52bc0fa113fef
william mpassy <williammpassy@gmail.com>
2020-01-12 21:46:01 +0100

    william mpassy push travail colombe singbeu

---
+++ View/View_deconnexion.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title></title>
+
+  <meta charset="UTF-8"/>
+  
+</head>
+
+<body>
+
+          <form id="frm_newsletter" method="post" action="index.php">
+                  
+                  
+                      <select id="object" name="object">
+                          <option value="unsubscribe">Désinscription</option>
+                      </select>
+      
+
+                  
+
+              
+          </form>
+
+</body> 
+</html>
