842b921309f1be8104affbeaef5a57fab3d48610
Sorli Pierre <bert.dud2011@gmail.com>
2020-01-13 16:30:44 +0100

    création du cookie utilisateur par Pierre Sorli

--- Model/Modele_connexion.php
+++ Model/Modele_connexion.php
@@ -14,13 +14,18 @@ $donnees = $reponse->fetchall();
 $trouve=false;
 //print_r ($donnees);
 for ($i=0;$i<count($donnees);$i++){
-    if($donnees[$i][4]===$_GET["adresse_mail"]&&$donnees[$i][5]===$_GET["mdp"]){
+    if($donnees[$i][5]===$_GET["adresse_mail"]&&$donnees[$i][4]===$_GET["mdp"]){
         $trouve=true;
         $reponse2 = $donnees[$i][2];
     }
+    
 }
 if ($trouve){
-    echo "<input type='hidden' name='prenom_connexion' value='$reponse2'>";
+    
+    //echo "<input type='hidden' name='prenom_connexion' method='GET' value='$reponse2'>";
+    setcookie("prenom_connexion", $reponse2, time()+3600,"/","index.php");
+    
     echo "c'est cool";
+    header ("location:View_accessoire_bague.php");
 }else {echo "pas cool t'existe pas";}
 ?>      
