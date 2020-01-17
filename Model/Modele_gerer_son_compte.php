688b42868024475e64288ec953a4a9b556a163b3
Sorli Pierre <bert.dud2011@gmail.com>
2020-01-17 14:29:08 +0100

    petite modif histoire de Pierre Sorli

--- Model/Modele_gerer_son_compte.php
+++ Model/Modele_gerer_son_compte.php
@@ -1,17 +1,19 @@
 <?php 
 $bdd = new PDO("mysql:host=localhost;dbname=stealmarket","root","");
 $id = $_COOKIE['ID_connexion'];
-$reponse = $bdd->query("SELECT * from utilisateurs WHERE ID = ".$id."");
-echo($id);
-$row = $reponse->fetch(); 
-
 if(isset($_GET['modif'])){
     $mdp=$_GET["mdp"];
     $bdd->query("UPDATE utilisateurs SET mdp = '".$mdp."' WHERE ID = ".$id."");
-    echo"votre mot de passe a bien été modifié";
+    
 } else if(isset($_GET['suppr'])){
     $bdd->query("DELETE * FROM utilisateurs WHERE ID = ".$id."");
     echo"votre compte a été supprimé vous n'existez plus pour nous";
+    echo"votre mot de passe a bien été modifié";
 }
+$reponse = $bdd->query("SELECT * from utilisateurs WHERE ID = ".$id."");
+//echo($id);
+$row = $reponse->fetch(); 
+
+
 
 ?>
