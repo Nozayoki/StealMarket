924d4b4348bdbaf3f0faa1effc0a0b90226d284a
Sorli Pierre <bert.dud2011@gmail.com>
2020-01-30 23:46:21 +0100

    voir article

--- View/View_article.php
+++ View/View_article.php
@@ -16,11 +16,14 @@ include("../View/View_accueil_stealmarket.php")
    <h1><?= $contenu ?></h1>
    <form method='POST'>
    </p>
-   <p><img src="miniatures/<?= $id ?>.jpg" width="400" /><p>
+   <?php if($articles['src']=='nosrc' ){$src="miniatures/". $articles['ID'];}else {$src=$articles['src'];}?>
+                  <p>  <img src="<?php echo $src; ?>.jpg" width="100" /></p>
    <select name='nombre_article'>
-    <option value='1' selected>1</option>
-    <option value='2' selected>2</option>
-    <option value='3' selected>3</option>
+      <?php
+      for ($lo=1;$lo<$articles['stock']+1;$lo++){
+         echo "<option value='$lo' selected>$lo</option>";
+      }
+      ?>
     <?php $azer=$_GET["ID"]; echo "<input type='hidden' name='ID' value='$azer'"?>>
         <input type="submit" name="Panier" value="Ajouter Au Panier">
         </form>
